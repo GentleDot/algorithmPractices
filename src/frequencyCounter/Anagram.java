65acb359d4590e5ada8bc43bdf1e1d8b9099e92c
gentledot <gentledot.wp@gmail.com>
2020-04-07 19:26:44 +0900

    algorithmPractice - Understanding common problem solving patterns_Frequency Counter

---
+++ src/frequencyCounter/Anagram.java
@@ -0,0 +1,55 @@
+package frequencyCounter;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Anagram {
+    public static void main(String[] args) {
+        System.out.println(checkVaildAnagram("", "")); // true
+        System.out.println(checkVaildAnagram("aaz", "zza")); // false
+        System.out.println(checkVaildAnagram("anagram", "nagaram")); // true
+        System.out.println(checkVaildAnagram("rat", "car")); // false
+        System.out.println(checkVaildAnagram("awesome", "awesom")); // false
+        System.out.println(checkVaildAnagram("qwerty", "qeywrt")); // true
+        System.out.println(checkVaildAnagram("texttwisttime", "timetwisttext")); // true
+        System.out.println(checkVaildAnagram("iceman", "cinema")); // true
+
+    }
+
+    public static boolean checkVaildAnagram(String target, String madeOne) {
+        // target 글자 수와 madeOne 글자 수가 같지 않으면 false
+        int targetLength = target.length();
+        int madeOneLength = madeOne.length();
+
+        if (targetLength != madeOneLength) {
+            return false;
+        }
+
+        // target, madeOne을 map으로 변환 (targetMap, madeOneMap)
+        Map<Character, Long> targetMap = target.chars()
+                .mapToObj(value -> (char) value)
+                .collect(Collectors.groupingBy(character -> character,
+                        Collectors.counting()));
+
+        /*Map<Character, Long> madeOneMap = madeOne.chars()
+                .mapToObj(value -> (char) value)
+                .collect(Collectors.groupingBy(character -> character,
+                        Collectors.counting()));*/
+
+        // 글자 확인
+        // targetMap의 key가 madeOneMap의 key에 없으면 false
+        // targetMap의 key로 가져온 개수가 madeOneMap의 key로 가져온 개수와 같지 않다면 false
+        for (char key : targetMap.keySet()) {
+            if (!madeOne.contains(String.valueOf(key))) {
+                return false;
+            } else if (targetMap.get(key) <= 0) {
+                return false;
+            }
+            targetMap.replace(key, targetMap.get(key) - madeOne.chars().filter(value -> value == key).count());
+        }
+
+        // 특이 사항이 없다면 true 반환
+        System.out.println(targetMap);
+        return targetMap.values().stream().noneMatch(value -> value > 0);
+    }
+}
---
+++ src/frequencyCounter/SameCheck.java
@@ -0,0 +1,77 @@
+package frequencyCounter;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class SameCheck {
+    public static void main(String[] args) {
+//        System.out.println(same(new int[]{1, 2, 3, 2}, new int[]{9, 1, 4, 4}));
+        System.out.println(sameRefactored(new int[]{1, 2, 3, 2}, new int[]{9, 1, 4, 4}));
+
+    }
+
+    public static boolean same(int[] arr1, int[] arr2) {
+        int arr1Length = arr1.length;
+        int arr2Length = arr2.length;
+
+        if (arr1Length != arr2Length) {
+            return false;
+        }
+
+        List<Integer> arr2List = Arrays.stream(arr2)
+                .boxed()
+                .collect(Collectors.toList());
+
+
+        for (int i = 0; i < arr1Length; i++) {
+            int arr1Item = arr1[i];
+            Optional<Integer> matchedArr2Item = arr2List.stream().filter(value -> value == Math.pow(arr1Item, 2)).findFirst();
+
+            if (matchedArr2Item.isEmpty()) {
+                return false;
+            }
+            System.out.println(arr2List);
+            arr2List.remove(matchedArr2Item.get());
+        }
+
+        return true;
+    }
+
+    public static boolean sameRefactored(int[] arr1, int[] arr2) {
+        int arr1Length = arr1.length;
+        int arr2Length = arr2.length;
+
+        if (arr1Length != arr2Length) {
+            return false;
+        }
+
+        /*Map<Integer, Long> arr1Map = Arrays.stream(arr1)
+                .boxed()
+                .collect(Collectors.groupingBy(item -> item,
+                        Collectors.counting()));*/
+
+        Map<Integer, Integer> arr1Map = Arrays.stream(arr1)
+                .boxed()
+                .collect(Collectors.groupingBy(item -> item,
+                        Collectors.reducing(0, value -> 1, Integer::sum)));
+
+        Map<Integer, Integer> arr2Map = Arrays.stream(arr2)
+                .boxed()
+                .collect(Collectors.toMap(item -> item, value -> 1, Integer::sum));
+
+        for (int key : arr1Map.keySet()) {
+            if (!arr2Map.containsKey((int) Math.pow(key, 2))) {
+                return false;
+            } else if (!arr2Map.get((int) Math.pow(key, 2)).equals(arr1Map.get(key))) {
+                return false;
+            }
+        }
+
+        System.out.println(arr1Map);
+        System.out.println(arr2Map);
+        return true;
+    }
+}
