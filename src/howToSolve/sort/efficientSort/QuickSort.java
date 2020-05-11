85dbdd049cebf23c6ad75674c9ee2c08fdf175c0
gentledot <gentledot.wp@gmail.com>
2020-05-11 22:36:03 +0900

    algorithmPractice - 정렬(sort)_quickSort : pivot (배열 재배치) 기능이 적용된 quickSort 구현

--- src/howToSolve/sort/efficientSort/QuickSort.java
+++ src/howToSolve/sort/efficientSort/QuickSort.java
@@ -1,10 +1,45 @@
 package howToSolve.sort.efficientSort;
 
+import java.util.Arrays;
+
 import static howToSolve.Main.log;
 
 public class QuickSort {
     public static void main(String[] args) {
-        System.out.println(pivot(new int[]{4, 8, 2, 1, 5, 7, 6, 3}, null, null));
+//        System.out.println(pivot(new int[]{4, 8, 2, 1, 5, 7, 6, 3}, null, null));
+        System.out.println(Arrays.toString(quickSort(new int[]{4, 6, 9, 1, 2, 5, 3}, null, null)));
+//        System.out.println(Arrays.toString(quickSort(new int[]{100, -3, 4, 6, 9, 1, 2, 5, 3}, null, null)));
+    }
+
+    public static int[] quickSort(int[] array, Integer left, Integer right) {
+        int length = array.length;
+
+        // left와 right가 지정되지 않은 경우 left = 0, right = (length - 1) 로 설정
+        if (left == null) {
+            left = 0;
+        }
+        if (right == null) {
+            right = length - 1;
+        }
+
+        // pivot index를 기준으로
+            // 좌측은 start index보다 (pivot index - 1)이 큰 경우 정렬 진행
+            // 우측은 pivot index + 1 보다 end index가 큰 경우 정렬 진행
+        if (left < right) {
+            int pivotIndex = pivot(array, left, right);
+            log.debug("pivot index : {}, pivot = {}", pivotIndex, array[pivotIndex]);
+            log.debug("left index : {}, left = {}", left, array[left]);
+            log.debug("right index : {}, right = {}", right, array[right]);
+
+            // left 정렬
+            quickSort(array, left, pivotIndex - 1);
+            log.debug("left 정렬 후 array : {}", array);
+            // right 정렬
+            quickSort(array, pivotIndex + 1, right);
+            log.debug("right 정렬 후 array : {}", array);
+        }
+
+        return array;
     }
 
     // 배열, 배열의 시작 index, 배열의 종료 index를 parameter로 전달 받음
@@ -29,8 +64,8 @@ public class QuickSort {
         // 배열의 처음 index부터 종료 index까지 loop 진행
         for (int i = start + 1; i <= end; i++) {
             // pivot이 탐색 중인 값보다 큰 경우
-                // pivot index += 1
-                // 탐색 된 값과 pivot 값을 swap
+            // pivot index += 1
+            // 탐색 된 값과 pivot 값을 swap
             if (pivot > array[i]) {
                 swapIndex++;
                 swap(array, swapIndex, i);
