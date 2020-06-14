fcdf708f819badb22d0824a4ab58573fca6ab87a
gentledot <gentledot.wp@gmail.com>
2020-06-14 21:36:20 +0900

    algorithmPractice - dataStructures : Hash Table_get(), set() 구현

---
+++ src/dataStructure/hashTable/HashTable.java
@@ -0,0 +1,83 @@
+package dataStructure.hashTable;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static howToSolve.Main.log;
+
+public class HashTable {
+    private final int primeNumber = 31;
+
+    private List<List<String[]>> keyMap;
+    private int keyMapSize;
+
+    public HashTable() {
+        this(53);
+    }
+
+    public HashTable(int size) {
+        this.keyMapSize = size;
+        this.keyMap = new ArrayList<>(size);
+        for (int i = 0; i < size; i++) {
+            this.keyMap.add(new ArrayList<>());
+        }
+    }
+
+    public List<List<String[]>> getKeyMap() {
+        return keyMap;
+    }
+
+    public int getKeyMapSize() {
+        return keyMapSize;
+    }
+
+    public Integer hash(String key) {
+        int total = 0;
+        int length = key.length();
+        for (int i = 0; i < Math.min(length, 100); i++) {
+            char charValue = key.charAt(i);
+            int value = charValue - 96;
+
+//            log.info("value : {} // total * primeNumber + value = {}", value, ((total * primeNumber) + value));
+            total = ((total * primeNumber) + value) % keyMapSize;
+//            log.info("total = {}", total);
+        }
+
+        return total;
+    }
+
+    public void set(String key, String value) {
+        int index = hash(key);
+        log.info("key hashing : {}", index);
+
+        if (this.keyMap.get(index) == null) {
+            this.keyMap.set(index, new ArrayList<>());
+        }
+
+        this.keyMap.get(index).add(new String[]{key, value});
+    }
+
+    public String[] get(String key) {
+        int index = hash(key);
+        log.info("key hashing : {}", index);
+        List<String[]> strings = this.keyMap.get(index);
+        if (!strings.isEmpty()) {
+            return strings.stream().filter(strList -> strList[0].equals(key)).findFirst().orElse(null);
+        }
+
+        return null;
+    }
+
+    @SuppressWarnings("unchecked")
+    public int findCapacity() throws Exception {
+
+        Field field = this.keyMap.getClass().getDeclaredField("elementData");
+
+        field.setAccessible(true);
+
+        return ((Object[]) field.get(this.keyMap)).length;
+
+    }
+}
--- src/dataStructure/hashTable/Main.java
+++ src/dataStructure/hashTable/Main.java
@@ -1,13 +1,30 @@
 package dataStructure.hashTable;
 
+import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args) {
-        Hash hash = new Hash();
+    public static void main(String[] args) throws Exception {
+//        Hash hash = new Hash();
         /*System.out.println(hash.oldHash("pink", 10));
         System.out.println(hash.oldHash("orangered", 10));
         System.out.println(hash.oldHash("cyan", 10));*/
-        System.out.println(hash.hash("hello", 13));
+        /*System.out.println(hash.hash("hello", 13));
         System.out.println(hash.hash("goodbye", 13));
-        System.out.println(hash.hash("cyan", 13));
+        System.out.println(hash.hash("cyan", 13));*/
+
+        HashTable table = new HashTable();
+        /*System.out.println(table.getKeyMap().size());
+        System.out.println(table.getKeyMap().toArray().length);*/
+//        System.out.println(table.findCapacity());
+        table.set("hello world", "goodbye!!");
+        System.out.println(Arrays.toString(table.getKeyMap().get(40).get(0)));
+        System.out.println(Arrays.toString(table.get("hello world")));
+
+        HashTable colorTable = new HashTable(17);
+        colorTable.set("maroon", "#800000");
+        colorTable.set("yellow", "#FFFF00");
+        colorTable.set("lightcoral", "#F08080");
+        System.out.println("======");
+        System.out.println(Arrays.toString(colorTable.get("yellow")));
     }
 }
